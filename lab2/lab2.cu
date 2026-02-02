32f61a7f3d71fe67601676b314b660918c7a9f06
Muhammad Omer Sajid <msajid@dt-login03.delta.ncsa.illinois.edu>
2026-02-01 23:34:48 -0600

    add comments

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -22,6 +22,7 @@ __global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
   int row = threadIdx.y + (blockDim.y * blockIdx.y);
   int col = threadIdx.x + (blockDim.x * blockIdx.x);
 
+  // c_row_col = a_row_k * b_k_col
   if (col < numCColumns && row < numCRows) {
     float elemVal = 0;
     int K = numBRows;
@@ -38,7 +39,7 @@ int main(int argc, char **argv) {
   float *hostA; // The A matrix
   float *hostB; // The B matrix
   float *hostC; // The output C matrix
-  
+
   int numARows;    // number of rows in the matrix A
   int numAColumns; // number of columns in the matrix A
   int numBRows;    // number of rows in the matrix B
@@ -56,12 +57,11 @@ int main(int argc, char **argv) {
                             &numBColumns);
   wbLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
   wbLog(TRACE, "The dimensions of B are ", numBRows, " x ", numBColumns);
-  
+
   //@@ Set numCRows and numCColumns
   numCRows = numARows;
   numCColumns = numBColumns;
   wbLog(TRACE, "The dimensions of C are ", numCRows, " x ", numCColumns);
-
 
   //@@ Allocate the hostC matrix
   int cElems = numCRows * numCColumns;
@@ -72,28 +72,26 @@ int main(int argc, char **argv) {
   float *deviceA;
   float *deviceB;
   float *deviceC;
- 
+
   cudaMalloc((void**) &deviceA, numARows * numAColumns * sizeof(float));
   cudaMalloc((void**) &deviceB, numBRows * numBColumns * sizeof(float));
   cudaMalloc((void**) &deviceC, numCRows * numCColumns * sizeof(float));
-
 
   //@@ Copy memory to the GPU here
   cudaMemcpy(deviceA, hostA, numARows * numAColumns * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(deviceB, hostB, numBRows * numBColumns * sizeof(float), cudaMemcpyHostToDevice);
 
-
   //@@ Initialize the grid and block dimensions here
-  dim3 threadsPerBlock(16, 16);
+  int BLOCK_SIZE = 16;
+  dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
   dim3 blocks(std::ceil((float) numCColumns / (float) threadsPerBlock.x), std::ceil((float) numCRows / (float) threadsPerBlock.y));
 
   //@@ Launch the GPU Kernel here
-  matrixMultiply<<<blocks, threadsPerBlock>>>(deviceA, deviceB, deviceC, 
-    numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
+  matrixMultiply<<<blocks, threadsPerBlock>>>(deviceA, deviceB, deviceC, numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
 
   cudaError_t error = cudaDeviceSynchronize();
   wbCheck(error);
-  
+
   //@@ Copy the GPU memory back to the CPU here
   cudaMemcpy(hostC, deviceC, cSize, cudaMemcpyDeviceToHost);
 
@@ -107,6 +105,7 @@ int main(int argc, char **argv) {
   free(hostA);
   free(hostB);
   //@@Free the hostC matrix
+  free(hostC);
 
   return 0;
 }
